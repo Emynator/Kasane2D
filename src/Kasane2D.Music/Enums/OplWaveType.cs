a0fb9400f4b3d7daaa85fd4077bee5a8fa8a74a2
Emynator <12451038+Emynator@users.noreply.github.com>
2026-08-27 22:25:23 +0200

    xml docs for OplWaveType

--- src/Kasane2D.Music/Enums/OplWaveType.cs
+++ src/Kasane2D.Music/Enums/OplWaveType.cs
@@ -1,9 +1,26 @@
+using Kasane2D.Music.Synthesis.Generators;
+
 namespace Kasane2D.Music.Enums;
 
+/// <summary>
+/// Wave type for <see cref="Opl2Voice"/>'s operators.
+/// </summary>
 public enum OplWaveType
 {
+    /// <summary>
+    /// Sine wave.
+    /// </summary>
     Sine,
+    /// <summary>
+    /// Positive half of a sine wave.
+    /// </summary>
     HalfSine,
+    /// <summary>
+    /// Sine wave with negative half folded to positive.
+    /// </summary>
     AbsSine,
+    /// <summary>
+    /// Sine wave with negative half folded to positive and only the first and third quarter of the wave.
+    /// </summary>
     SineSaw,
 }
